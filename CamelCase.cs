abe65b1442a6985d564a223b50b453c9588c13a7
eduardo1997 <edumoy_cr1997@hotmail.com>
2016-10-12 15:32:43 -0600

    Add files via upload

---
+++ CamelCase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1Progra3EduardoMontes
+{
+    //Eduardo Montes de Oca Yglesias
+    class CamelCase
+    {
+        //metodo calculate que retornara un int y resive por parametro un String
+        public int calculate(String palabra) { 
+            //integer que retornara la cantidad de palabras que hay en una frase
+            int cantidadPalabras = 0;
+            //for que recorera la frase o palabra que sera digitado por el usuario sin importar su longitud  
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                //condicion que detectara si hay mayusculas y que aumente la variable cantidadPalabras 
+                if (char.IsUpper(palabra[i]))
+                {
+                    cantidadPalabras++;
+                }
+            }
+            //condicion que detectara si hay minusculas y que aumente la variable cantidadPalabras
+            //va fuera del for porque si no hagarraria toda las letras que esten en minuscula aumentando considerablemente cantidadPalabras y eso no es lo que queremos
+            if (char.IsLower(palabra[0]))
+            {
+                cantidadPalabras++;
+            }
+            //retorne la cantidad de palabras ingresadas
+            return cantidadPalabras;
+        }
+
+    }
+}
---
+++ DiagonalDifference.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1Progra3EduardoMontes
+{   //Eduardo Montes de Oca Yglesias
+    class DiagonalDifference
+    {
+        //metodo matriz con un parametro String 
+        public void matrix(String numeroMatriz)
+        {
+            //arreglo de String que almacena "|" y separa el caracter especial con split
+            String[] arreglo = numeroMatriz.Split('|');
+            //matriz de int que almacena las filas y columnas
+            int[,] matriz2 = new int[arreglo.Length, arreglo.Length];
+            //for que recorre el arreglo
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                for (int a = 0; a < 1; a++)
+                {
+                    String[] separador = arreglo[i].Split(' ');
+                    //for que recorre el arreglo separador
+                    for (int b = 0; b < separador.Length; b++)
+                    {
+                        //convertira el String a entero y lo amacenara en la matriz 
+                        matriz2[i, b] = int.Parse(separador[b].ToString());
+                    }
+                }
+            }
+            //for que imprime las filas de la matriz
+            for (int i = 0; i < matriz2.GetLength(0); i++)
+            {
+                //for que imprime las columnas de la matriz 
+                for (int c = 0; c < matriz2.GetLength(1); c++)
+                {
+                    Console.Write(matriz2[i,c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
